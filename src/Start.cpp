b7566980e7a068b57e6b0fee0f1de39938aa75b0
LaTable007 <noah.dubuc@gmail.com>
2025-02-19 22:18:01 +0100

    Simulation Densité vérification que les valeurs sont logiques

--- src/Start.cpp
+++ src/Start.cpp
@@ -1,11 +1,12 @@
 #include "../include/Start.h"
 #include "../include/Balle.h"
+#include "../include/Box.h"
 #include <cmath>
 #include <iostream>
 #include <vector>
 
 void Start(std::vector<Balle> &balles, int numParticles, float ballRadius,
-           float spacing) {
+           float spacing, Box &box) {
   // Dimensions de la grille
   int particlesPerRow = std::sqrt(numParticles);
   int particlesPerCol = (numParticles + particlesPerRow - 1) / particlesPerRow;
@@ -15,14 +16,17 @@ void Start(std::vector<Balle> &balles, int numParticles, float ballRadius,
   float gridSpacing = ballRadius * 2 + spacing;
 
   // Plages de coordonnées
-  float minX = 50.f;
-  float maxX = 750.f;
-  float minY = 50.f;
-  float maxY = 550.f;
+  float left, right, up, down;
+  box.getBounds(&left, &right, &up, &down);
+  float maxx = right - 2 * ballRadius;
+  float minx = left + 2 * ballRadius;
+
+  float maxy = down - 2 * ballRadius;
+  float miny = up + 2 * ballRadius;
 
   // Calcul des espacements dans les limites données
-  float rangeX = maxX - minX;
-  float rangeY = maxY - minY;
+  float rangeX = maxx - minx;
+  float rangeY = maxy - miny;
 
   // Calcul du décalage pour centrer la grille
   float offsetX = (rangeX - (particlesPerRow - 1) * gridSpacing) / 2.f;
@@ -36,14 +40,14 @@ void Start(std::vector<Balle> &balles, int numParticles, float ballRadius,
     int col = i % particlesPerRow;
 
     // Position des balles dans la boîte, avec centrage
-    float x = minX + offsetX + (col * gridSpacing); // Espacement en x centré
-    float y = minY + offsetY + (row * gridSpacing); // Espacement en y centré
+    float x = minx + offsetX + (col * gridSpacing); // Espacement en x centré
+    float y = miny + offsetY + (row * gridSpacing); // Espacement en y centré
 
     // Vérifie si les balles ne sortent pas des limites
-    if (x + ballRadius > maxX)
-      x = maxX - ballRadius;
-    if (y + ballRadius > maxY)
-      y = maxY - ballRadius;
+    if (x + ballRadius > maxx)
+      x = maxx - ballRadius;
+    if (y + ballRadius > maxy)
+      y = maxy- ballRadius;
 
     balles.emplace_back(ballRadius, sf::Vector2f(x, y), sf::Vector2f(0, 0));
   }
